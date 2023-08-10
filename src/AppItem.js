4486dfefdc5236ce8e8f86cc9c5af1cb33e9d33c
bumjun2 <jbj3713@naver.com>
2023-08-10 14:30:39 +0900

    컴퍼넌트 기초

---
+++ src/AppItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AppItem extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>하하호호</h1>
+      </div>
+    );
+  }
+}
+
+export default AppItem;
---
+++ src/Hello.js
@@ -0,0 +1,8 @@
+// rafce
+import React from 'react';
+
+const Hello = () => {
+  return <div>Hello</div>;
+};
+
+export default Hello;
